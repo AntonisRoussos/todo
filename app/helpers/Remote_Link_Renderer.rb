92a37e9ba57138c6e5974ef3c31800f1bb19474b
Antonis Roussos <grtramp@yahoo.gr>
2011-09-18 23:53:34 +0300

    wtf5

--- app/helpers/Remote_Link_Renderer.rb
+++ app/helpers/Remote_Link_Renderer.rb
@@ -2,12 +2,7 @@ class RemoteLinkRenderer < WillPaginate::ViewHelpers::LinkRenderer
 
   def link(text,target,attributes={})
 	attributes ["data-remote"]=true
-        if target.is_a? Fixnum
-          attributes[:rel] = rel_value(target)
-          target = url(target)
-        end
-        attributes[:href] = target
-        tag(:a, text, attributes)
+	super
   end
 
 end
