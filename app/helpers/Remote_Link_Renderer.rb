010bd40134a205baa7ba1dbe95260eed4cedea48
Antonis Roussos <grtramp@yahoo.gr>
2011-09-18 22:19:52 +0300

    tabs1

--- app/helpers/Remote_Link_Renderer.rb
+++ app/helpers/Remote_Link_Renderer.rb
@@ -1,11 +1,12 @@
 class RemoteLinkRenderer < WillPaginate::ViewHelpers::LinkRenderer
-  def link(text,target,attributes={})
+#class RemoteLinkRenderer < WillPaginate::ActionView::LinkRenderer
+   def link(text,target,attributes={})
 	attributes ["data-remote"]=true
 #	oldurl = url(target)
 #	url(target) << "javascript:loadContent('#task_information',"+:oldurl+")"
 #	Rails.logger.debug("-----   ----------------------------------------url")
 #	Rails.logger.debug("#{url(target)}")
-	super
+#	super
   end
 
 end
